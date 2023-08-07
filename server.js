db0b75bad37aad7e70768122a5f086911860738c
UmamaheshMaxwell <devopswithkube@gmail.com>
2023-08-07 15:59:35 +0530

    added CI CD setup

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const cors = require("cors")
+const app = express()
+
+app.use(cors())
+
+app.get("/", (req, res) => {
+    res.json("Welcome to Node API")
+})
+
+app.get("/user", (req, res) => {
+    res.json([
+        { id: 1, name: "Scott Desatnick" },
+        { id: 2, name: "Adam Colson" },
+        { id: 3, name: "Tuan Bui" },
+        { id: 4, name: "Jagrav"}
+    ])
+})
+
+const PORT = 8080
+
+app.listen(PORT, () => {
+    console.log(`Server listening at PORT ${PORT}`)
+})
