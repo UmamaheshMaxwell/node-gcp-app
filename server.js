7c7dffc1e42276d73221de8c9b22b16a4593e5ac
UmamaheshMaxwell <devopswithkube@gmail.com>
2023-08-09 10:50:14 +0530

    manadated authentication

--- server.js
+++ server.js
@@ -2,13 +2,20 @@ const express = require("express")
 const cors = require("cors")
 const app = express()
 
+
 app.use(cors())
 
 app.get("/", (req, res) => {
+    res.set('Access-Control-Allow-Origin', '*');
+    res.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.set('Access-Control-Allow-Headers', 'Content-Type');
     res.json("Welcome to Node API")
 })
 
 app.get("/users", (req, res) => {
+    res.set('Access-Control-Allow-Origin', '*');
+    res.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.set('Access-Control-Allow-Headers', 'Content-Type');
     res.json([
         { id: 1, name: "Scott Desatnick" },
         { id: 2, name: "Adam Colson" },
