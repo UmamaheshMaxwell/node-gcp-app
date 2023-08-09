2c0d02058397c9441108525be15d184e27dcd564
UmamaheshMaxwell <devopswithkube@gmail.com>
2023-08-09 12:59:42 +0530

    added cors fix

--- server.js
+++ server.js
@@ -3,19 +3,22 @@ const cors = require("cors")
 const app = express()
 
 
-app.use(cors())
+
+app.use(cors({credentials: true, origin: '*'}));
+
+// Add CORS headers
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// });
 
 app.get("/", (req, res) => {
-    res.set('Access-Control-Allow-Origin', '*');
-    res.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-    res.set('Access-Control-Allow-Headers', 'Content-Type');
     res.json("Welcome to Node API")
 })
 
 app.get("/users", (req, res) => {
-    res.set('Access-Control-Allow-Origin', '*');
-    res.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-    res.set('Access-Control-Allow-Headers', 'Content-Type');
     res.json([
         { id: 1, name: "Scott Desatnick" },
         { id: 2, name: "Adam Colson" },
